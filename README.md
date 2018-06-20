83a418b55ce99e466ee8b81b7ba0fdfcc44696c9
Brian H <bheptin@gmail.com>
2018-06-20 12:49:54 -0500

    Add to README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SeleniumTest
+
+This test was produced using Node and the 'selenium-webdriver' npm package.
+Make sure to have Chrome installed and run: brew instal chromedriver
+in command line(Mac).
+
+to install Selenium: in command line enter: npm instal selenium-webdriver
+
+To run the test: change to directory "SeleniumTest" and enter node sample.js
--- sample.js
+++ sample.js
@@ -1,25 +1,29 @@
-var webdriver = require('selenium-webdriver');
-var {By, click} = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
-var driver = new webdriver.Builder().forBrowser('chrome').build();
-
-driver.get('https://developers.google.com');
-
-driver.findElement(By.id('google-cloud-next-18br-july-24-26-san-francisco-usa')).click();
-driver.sleep(5000);
-driver.findElement(By.xpath('//*[@id="main"]/span/div[1]/div/div/div/div[4]/button')).click();
- var searchForm = driver.findElement(By.tagName('form'));
- var searchBox = searchForm.findElement(By.name('q'));
- searchBox.sendKeys('webdriver');
- driver.findElement(By.name('btnK')).click();
-
-
-
-/*driver.get('https://developers.google.com');
-driver.sleep(10000);
-var paragraph = driver.findElement(webdriver.By.className('devsite-landing-row-item-description-content')).getText();
-if(paragraph.equals("Hone your skills on the latest cloud technologies with Google experts at hundreds of breakout sessions and interactive on-demand hands-on labs and bootcamps.  You'll have the opportunity to engage with the best minds in cloud technology on how your industry is adapting, innovating, and growing with cloud.")){
-    console.log("This is correct");
-} else {           { className: "devsite-landing-row-item-description-content" }
-  console.log("This is wrong!");
-};*/
+(async function test() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://developers.google.com');
+    await driver.findElement(By.xpath('//*[contains(text(), "Hone your skills on the latest cloud technologies")]')).isDisplayed();
+    await driver.findElement(By.id('google-cloud-next-18br-july-24-26-san-francisco-usa')).click();
+    await driver.sleep(5000);
+    await driver.findElement(By.name('firstName')).click();
+    await driver.sleep(2000);
+    await driver.findElement(By.name('firstName')).sendKeys('Brian', Key.RETURN);
+    await driver.sleep(2000);
+    await driver.findElement(By.name('lastName')).sendKeys('Heptinstall', Key.RETURN);
+    await driver.sleep(2000);
+    await driver.findElement(By.name('email')).sendKeys('bheptin@gmail.com', Key.RETURN);
+    await driver.sleep(2000);
+    await driver.findElement(By.name('jobTitle')).sendKeys('QA Tester', Key.RETURN);
+    await driver.sleep(2000);
+    await driver.findElement(By.name('company')).sendKeys('Influence Health', Key.RETURN);
+    await driver.sleep(2000);
+    await driver.findElement(By.xpath('//*[@id="industry"]/option[3]')).click();
+    await driver.sleep(2000);
+    await driver.findElement(By.xpath('//*[@id="country"]/option[232]')).click();
+    await driver.sleep(5000);
+  } finally {
+    await driver.quit();
+  }
+})();
