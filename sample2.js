80b731581bf87fe595604840e8205a3fc15c99c4
Brian H <bheptin@gmail.com>
2018-06-20 12:28:02 -0500

    start on form at bottom

--- sample2.js
+++ sample2.js
@@ -1,13 +1,19 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
-(async function example() {
+(async function test() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('https://developers.google.com');
-    await driver.findElement(By.xpath('//*[contains(text(), "Hone your skills on the latest cloud technologies with Google experts at hundreds of breakout sessions and interactive on-demand hands-on labs and bootcamps.  You\'ll have the opportunity to engage with the best minds in cloud technology on how your industry is adapting, innovating, and growing with cloud.")]/div[2]/article/article/section[1]/div/div[1]/div/div[1]/p[1]')).isDisplayed();
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.findElement(By.xpath('//*[contains(text(), "Hone your skills on the latest cloud technologies")]')).isDisplayed();
+    await driver.findElement(By.id('google-cloud-next-18br-july-24-26-san-francisco-usa')).click();
+    await driver.sleep(5000);
+    //await driver.findElement(By.cssSelector('btn.hero-cta-btn.light-blue.btn_border_white')).click();
+    //await driver.findElement(By.xpath('//*[contains(text(), "Imagine")]')).isDiplayed();
+    await driver.findElement(By.name('firstName')).click();
+    await driver.findElement(By.name('firstName')).sendKeys('Brian', Key.RETURN);
+    //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
     await driver.quit();
   }
 })();
+//*[@id="main"]/span/div[2]/div/div/div[1]/div[1]/div/div[1]/div[1]/h3
